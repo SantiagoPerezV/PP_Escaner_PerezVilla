8740aa52f364b465a585822588489886d376fef9
SantiagoPerezV <138243132+SantiagoPerezV@users.noreply.github.com>
2024-05-22 14:51:43 -0300

    Add files via upload

--- PP_Escaner_PerezVillaSantiago/Test/Program.cs
+++ PP_Escaner_PerezVillaSantiago/Test/Program.cs
@@ -1,11 +1,11 @@
 ﻿using Entidades;
-using Microsoft.Win32.SafeHandles;
-using PP_Escaner_PerezVillaSantiago;
-namespace Test;
-internal class Program
-{
-    static void Main(string[] args)
-    {
 
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
     }
 }
